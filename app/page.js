affac2136bac027a3036dcf672572eac141749df
risvinmpm <risvinmp39@gmail.com>
2025-06-20 15:38:13 +0530

    initial changes

--- app/page.js
+++ app/page.js
@@ -41,6 +41,45 @@ export default function Home() {
         },
       });
     });
+
+    cards.forEach((cards, index)=>{
+      const frontEl = cards.querySelector(".flip-card-front");
+      const backEl = cards.querySelector(".flip-card-back");
+
+      const staggerOffset = index * 0.05;
+      const startOffset = 1 / 3 + staggerOffset;
+      const endOffset = 2 / 3 + staggerOffset;
+
+
+      ScrollTrigger.create({
+        trigger: container.current.querySelector(".cards"),
+        start: "top top",
+        end: () => `+=${totalScrollHeight}`,
+        scrub: 1,
+        id: `rotate-flip-${index}`,
+        onUpdate: (self) => {
+          const progress = self.progress;
+          if(progress >= staggerOffset && progress <= endOffset) {
+            const animationProgress = (progress - startOffset) / ( 1 / 3 );
+            const frontRotation = -180 * animationProgress;
+            const backRotation = 180 - 180 * animationProgress;
+            const cardRotation = rotations[index] * ( 1 - animationProgress );
+
+
+            gsap.to(frontEl, { rotateY: frontRotation, ease: "power1.out"});
+            gsap.to(backEl, { rotateY: backRotation, ease: "power1.out"});
+            gsap.to(cards, {
+              xPercent: -50,
+              yPercent: -50,
+              rotate: cardRotation,
+              ease: "power1.out"
+            })
+          }
+        }
+      })
+    });
+    
+
   }, { scope: container });
 
   return (
